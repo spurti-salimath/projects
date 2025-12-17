5c9bc3848a97bb3500e9c7f955a2f21508cb8d7f
spurti-salimath <71642289+spurti-salimath@users.noreply.github.com>
2025-12-17 12:22:32 +0100

    Add files via upload

---
+++ MovieHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieOnlineStoreProject.Controllers
+{
+    public class MovieHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
